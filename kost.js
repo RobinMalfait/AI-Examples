d49538098bfa512631e39ebfb1e9e7faf8e69a21
Robin Malfait <malfait.robin@gmail.com>
2017-01-14 17:20:08 +0100

    standard

--- kost.js
+++ kost.js
@@ -23,22 +23,21 @@ const dataset = [
 /* OUTPUT */
 module.exports = {
   output: function output () {
-
     const thetas = [
       [-0.4, 1],
       [0.785, 0.425]
     ]
 
     thetas.forEach(([theta0, theta1], i) => {
-        console.log('Voorbeeld', i + 1)
-        console.log('-----------\n')
+      console.log('Voorbeeld', i + 1)
+      console.log('-----------\n')
 
-        console.log(`Theta0 = ${theta0}`)
-        console.log(`Theta1 = ${theta1}`)
-        console.log(`Heuristiek = ${theta0} + ${theta1}x`)
+      console.log(`Theta0 = ${theta0}`)
+      console.log(`Theta1 = ${theta1}`)
+      console.log(`Heuristiek = ${theta0} + ${theta1}x`)
 
-        console.log('\nKost', kost(J(theta0, theta1), dataset).toFixed(4))
-        console.log('')
+      console.log('\nKost', kost(J(theta0, theta1), dataset).toFixed(4))
+      console.log('')
     })
   }
 }
